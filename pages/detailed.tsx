d896a812c860e57dcfaf5fe3cb7c2b48d918c1cf
kokoblack <benzimakareem@yahoo.com>
2023-03-24 11:37:34 +0100

    fix deployment issue

--- pages/detailed.tsx
+++ pages/detailed.tsx
@@ -15,21 +15,13 @@ const Detailed = () => {
     setAddTodos,
     setMyTodos,
   } = useContext(TodoContext);
-
-  const todoName = myTodos[activeTodoIndex].name;
-  const taskName = myTodos[activeTodoIndex].active[activeTaskIndex].name;
-  const descText = myTodos[activeTodoIndex].active[activeTaskIndex].desc;
-  console.log(myTodos);
-
+  
+  const [todoName, setTodoName] = useState('');
+  const [taskName, setTaskName] = useState('');
+  const [descText, setDescText] = useState('');
   const [val, setVal] = useState(descText);
   const [done, toggleDone] = useState(false);
   const textAreaRef = useRef<HTMLTextAreaElement>(null!);
-
-  const resizeTextArea = () => {
-    textAreaRef.current.style.height = "auto";
-    textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
-    console.log(textAreaRef.current.scrollHeight);
-  };
 
   const UpdateActive = () => {
     const newObj = myTodos[activeTodoIndex].active.map((obj, ind) => {
@@ -62,84 +54,110 @@ const Detailed = () => {
     localStorage.setItem("myTodos", JSON.stringify(myTodos));
   };
 
-  useEffect(resizeTextArea, [val]);
+  useEffect(() => {
+    const getTodos = JSON.parse(localStorage.getItem("myTodos") || "[]");
+    if (getTodos === null || myTodos.length !== 0) {
+      localStorage.setItem("myTodos", JSON.stringify(myTodos));
+    } else {
+      myTodos === getTodos ? null : setMyTodos(getTodos);
+    }
+  }, [myTodos, setMyTodos]);
+
+  useEffect(() => {
+     if (myTodos.length !== 0) {
+    setTodoName(myTodos[activeTodoIndex].name)
+    setTaskName(myTodos[activeTodoIndex].active[activeTaskIndex].name)
+    setDescText(myTodos[activeTodoIndex].active[activeTaskIndex].desc)
+     }
+  }, [myTodos])
+
+  useEffect(() => {
+    if (textAreaRef.current !== null) {
+      textAreaRef.current.style.height = "auto";
+      textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
+    }
+  }, [val]);
 
   return (
-    <Box
-      bgColor="#EDF1D6"
-      position="absolute"
-      top="0"
-      w="full"
-      h="full"
-      maxW="1440px"
-      color="#40513B"
-    >
-      <Box
-        w="full"
-        h="full"
-        py={{ base: "5%", sm: "3%" }}
-        px="2%"
-        position="relative"
-      >
-        <NavigateBack page="/todo"/>
-
-        <Text
-          fontSize={{ base: "5vw", sm: "1rem" }}
-          bgColor="#EDF1D6"
-          px={{ base: "4vw", sm: "1rem" }}
-          py={{ base: "1vw", sm: ".5rem" }}
-          borderLeftRadius={{ base: "6vw", sm: "3xl" }}
-          borderRightRadius={{ base: "6vw", sm: "3xl" }}
-          borderColor="#609966"
-          border="1px"
-          w="max-content"
-          my="2rem"
-        >
-          {todoName}
-        </Text>
-
-        <Text
-          fontSize={{ base: "16vw", sm: "4rem", lg: "5rem" }}
-          lineHeight={{ base: "14vw", sm: "4rem", lg: "5rem" }}
-          my="3%"
-        >
-          {taskName}
-        </Text>
-
-        <Text
-          fontSize={{ base: "4vw", sm: ".8rem" }}
-          mb="1rem"
-          mt="2rem"
-          color="#405136"
-          opacity="0.8"
-        >
-          Additional Description
-        </Text>
-
-        <Textarea
-          bgColor="#EDF1D6"
-          placeholder="Click here to add discription"
-          _placeholder={{ color: "#40513B" }}
-          variant="unstyled"
-          w="full"
-          resize="none"
-          rows={1}
-          ref={textAreaRef}
-          value={val}
-          onChange={onChange}
-        />
-
-        <Box h="20vh" />
-
+    <>
+      {myTodos.length !== 0 && (
         <Box
-          onClick={() => {
-            toggleDone((prev) => !prev);
-          }}
+          bgColor="#EDF1D6"
+          position="absolute"
+          top="0"
+          w="full"
+          h="full"
+          maxW="1440px"
+          color="#40513B"
         >
-          {!done ? <SetAsDone /> : <Done />}
+          <Box
+            w="full"
+            h="full"
+            py={{ base: "5%", sm: "3%" }}
+            px="2%"
+            position="relative"
+          >
+            <NavigateBack page="/todo" />
+
+            <Text
+              fontSize={{ base: "5vw", sm: "1rem" }}
+              bgColor="#EDF1D6"
+              px={{ base: "4vw", sm: "1rem" }}
+              py={{ base: "1vw", sm: ".5rem" }}
+              borderLeftRadius={{ base: "6vw", sm: "3xl" }}
+              borderRightRadius={{ base: "6vw", sm: "3xl" }}
+              borderColor="#609966"
+              border="1px"
+              w="max-content"
+              my="2rem"
+            >
+              {todoName}
+            </Text>
+
+            <Text
+              fontSize={{ base: "16vw", sm: "4rem", lg: "5rem" }}
+              lineHeight={{ base: "14vw", sm: "4rem", lg: "5rem" }}
+              my="3%"
+            >
+              {taskName}
+            </Text>
+
+            <Text
+              fontSize={{ base: "4vw", sm: ".8rem" }}
+              mb="1rem"
+              mt="2rem"
+              color="#405136"
+              opacity="0.8"
+            >
+              Additional Description
+            </Text>
+
+            <Textarea
+              bgColor="#EDF1D6"
+              placeholder="Click here to add discription"
+              _placeholder={{ color: "#40513B" }}
+              variant="unstyled"
+              w="full"
+              resize="none"
+              rows={1}
+              ref={textAreaRef}
+              value={val}
+              onChange={onChange}
+            />
+
+            <Box h="20vh" />
+
+            <Box
+              onClick={() => {
+                toggleDone((prev) => !prev);
+              }}
+            >
+              {!done ? <SetAsDone /> : <Done />}
+            </Box>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      )}
+    </>
   );
 };
 
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,4 +1,4 @@
-import { Flex, Spacer, Box, Text, Grid, Input} from "@chakra-ui/react";
+import { Flex, Spacer, Box, Text, Grid, Input } from "@chakra-ui/react";
 import { useState, useContext, useEffect, useRef } from "react";
 import { TodoContext } from "@/components/TodoContext";
 import Link from "next/link";
@@ -21,7 +21,6 @@ const Todo = () => {
   console.log(myTodos[activeTodoIndex]);
 
   const [taskName, setTaskName] = useState("");
-  // const taskName = useRef('')
 
   const setTodos = () => {
     setMyTodos((prev) => {
@@ -44,92 +43,100 @@ const Todo = () => {
   };
 
   useEffect(() => {
+    const getTodos = JSON.parse(localStorage.getItem("myTodos") || "[]");
+    if (getTodos === null || myTodos.length !== 0) {
       localStorage.setItem("myTodos", JSON.stringify(myTodos));
-  }, [myTodos]);
+    } else {
+      myTodos === getTodos ? null : setMyTodos(getTodos);
+    }
+  }, [myTodos, setMyTodos]);
 
   return (
     <>
-    {/* <Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => ref.current = e.target.value }/> */}
-      <Box
-        px="4%"
-        py="2%"
-        bgColor="#EDF1D6"
-        color="#40513B"
-        maxW="1440px"
-        mx="auto"
-      >
-        <NavigateBack page='/'/>
-
-        <Text
-          fontSize={{ base: "20vw", sm: "15vw", lg: "10rem" }}
-          lineHeight={{ base: "18vw", sm: "13vw", lg: "10rem" }}
-          mb="3rem"
-          mt="2rem"
-        >
-          {myTodos[activeTodoIndex].name}
-        </Text>
-
-        <Flex
-          justifyContent="start"
-          alignItems="center"
-          px=".5%"
-          fontSize={{ base: "7vw", sm: "2rem" }}
+      {myTodos.length !== 0 && (
+        <Box
+          px="4%"
+          py="2%"
+          bgColor="#EDF1D6"
           color="#40513B"
+          maxW="1440px"
+          mx="auto"
         >
-          <ActiveTask num={myTodos[activeTodoIndex].active.length} />
+          <NavigateBack page="/" />
 
-          <Spacer />
-
-          <Flex justifyContent="start" alignItems="center" gap="4%">
-            <Buttons>Active</Buttons>
-            <Buttons>Done</Buttons>
-          </Flex>
-        </Flex>
-
-        {myTodos[activeTodoIndex].active.length === 0 ? (
-          <Createtask>task</Createtask>
-        ) : (
-          <Grid
-            templateColumns={{ base: "1fr", lg: "1fr 1fr" }}
-            placeItems="center"
-            columnGap="2%"
-            mt="1rem"
+          <Text
+            fontSize={{ base: "20vw", sm: "15vw", lg: "10rem" }}
+            lineHeight={{ base: "18vw", sm: "13vw", lg: "10rem" }}
+            mb="3rem"
+            mt="2rem"
           >
-            {myTodos[activeTodoIndex].active.map((todo, id) => (
-              <Box
-                key={id}
-                w="100%"
-                mt="2%"
-                borderLeftRadius={{ base: "8vw", sm: "2rem" }}
-                borderRightRadius={{ base: "8vw", sm: "2rem" }}
-                bgColor="#9DC08B"
-                p={{ base: "6%", sm: "3%" }}
-              >
-                <Link onClick={() => setActiveTaskIndex(id)} href="/detailed">
-                  <Text fontSize={{ base: "8vw", sm: "3rem" }}>
-                    {todo.name}
-                  </Text>
-                  <Text fontSize={{ base: "4vw", sm: "1.2rem" }}>
-                    {todo.desc === "" ? "Click to add description" : todo.desc}
-                  </Text>
-                </Link>
-              </Box>
-            ))}
-          </Grid>
-        )}
+            {myTodos[activeTodoIndex].name}
+          </Text>
 
-        {!addTodo && <AddButton setState={setAddTodos} />}
+          <Flex
+            justifyContent="start"
+            alignItems="center"
+            px=".5%"
+            fontSize={{ base: "7vw", sm: "2rem" }}
+            color="#40513B"
+          >
+            <ActiveTask num={myTodos[activeTodoIndex].active.length} />
 
-        {addTodo && (
-          <AddNewTodo
-            setAddTodo={setAddTodos}
-            setTodo={setTodos}
-            setTodoName={setTaskName}
-            name={taskName}
-            dep={addTodo}
-          />
-        )}
-      </Box>
+            <Spacer />
+
+            <Flex justifyContent="start" alignItems="center" gap="4%">
+              <Buttons>Active</Buttons>
+              <Buttons>Done</Buttons>
+            </Flex>
+          </Flex>
+
+          {myTodos[activeTodoIndex].active.length === 0 ? (
+            <Createtask>task</Createtask>
+          ) : (
+            <Grid
+              templateColumns={{ base: "1fr", lg: "1fr 1fr" }}
+              placeItems="center"
+              columnGap="2%"
+              mt="1rem"
+            >
+              {myTodos[activeTodoIndex].active.map((todo, id) => (
+                <Box
+                  key={id}
+                  w="100%"
+                  mt="2%"
+                  borderLeftRadius={{ base: "8vw", sm: "2rem" }}
+                  borderRightRadius={{ base: "8vw", sm: "2rem" }}
+                  bgColor="#9DC08B"
+                  p={{ base: "6%", sm: "3%" }}
+                >
+                  <Link onClick={() => setActiveTaskIndex(id)} href="/detailed">
+                    <Text fontSize={{ base: "8vw", sm: "3rem" }}>
+                      {todo.name}
+                    </Text>
+                    <Text fontSize={{ base: "4vw", sm: "1.2rem" }}>
+                      {todo.desc === ""
+                        ? "Click to add description"
+                        : todo.desc}
+                    </Text>
+                  </Link>
+                </Box>
+              ))}
+            </Grid>
+          )}
+
+          {!addTodo && <AddButton setState={setAddTodos} />}
+
+          {addTodo && (
+            <AddNewTodo
+              setAddTodo={setAddTodos}
+              setTodo={setTodos}
+              setTodoName={setTaskName}
+              name={taskName}
+              dep={addTodo}
+            />
+          )}
+        </Box>
+      )}
     </>
   );
 };
